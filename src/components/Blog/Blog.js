ca9285f6901692572e9cc7e8a256654433334b83
Golovaciuk Andrew <a.golovaciuk@autodoc.eu>
2022-10-25 18:38:23 +0300

    Add API

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,46 +1,83 @@
-import "./blog.css";
 import { Component } from "react";
 import { PostItem } from "../PostItem/PostItem";
-import { posts } from "../../share/projectData";
+// import { posts } from "../../share/projectData"; // Тут храняться посты локально
 import { showGetPosts, showText} from "../../share/projectLogic";
 import {Users} from "../Users/Users";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
 import AddForm from "./component/AddForm";
+import axios from "axios"
+
+import {Container, Row, Col, ListGroup, Button, Spinner} from 'react-bootstrap';
 
 
 
 export class Blog extends Component {
-    state = {
-        showUsers: true,
-        blogArr: JSON.parse(localStorage.getItem('blogPostBvs')) || posts // Тут масив который в локал сторадж заходит или который в share/projectData
-    }
 
-    likePosts = (pos) => {
-        const temp = [...this.state.blogArr]; // Перемменая с нашим масивом копируем сылку
-        // temp[pos].likeCount++; // Добавляем в масив  +1
-        temp[pos].liked = !temp[pos].liked // Меняем в нашем скопированом масиве
-        this.setState({
-            blogArr: temp // Меняем масив state на новый
-        })
-        localStorage.setItem('blogPostBvs', JSON.stringify(temp)); // Заносим масив в локалсторадже под названием blogPostBvs
-    }
-
-    DeletePost = pos => {
-        if(window.confirm('Вы уверены')){
-            const temp = [...this.state.blogArr];
-            temp.splice(pos, 1)
-            this.setState({
-                blogArr: temp // Меняем масив state на новый
-            })
-            localStorage.setItem('blogPostBvs', JSON.stringify(temp)); // Заносим масив в локалсторадже под названием blogPostBvs
+    constructor(props) {
+        super(props);
+        this.state ={
+            showUsers: true,
+            // blogArr: JSON.parse(localStorage.getItem('blogPostBvs')) || posts
+            blogArr: []
         }
-        
     }
 
+    componentDidMount() {
+        this.GetPosts();
+    }
+    componentWillMount() {
+    }
+
+    GetPosts = () => {
+        axios.get('https://634fe5de78563c1d82b2e4e2.mockapi.io/posts')
+            .then((response) => {
+                this.setState({
+                    blogArr: response.data
+                })
+            })
+    }
+
+    // Метод Ставим лайк для поста
+    likePosts = (pos) => {
+        const temp = [...this.state.blogArr];                           // Перемменая с нашим масивом копируем сылку
+        // temp[pos].likeCount++;                                       // Добавляем в масив  +1
+        temp[pos].liked = !temp[pos].liked                              // Меняем в нашем скопированом масиве liked
+        this.setState({
+            blogArr: temp                                               // Меняем масив state на новый
+        })
+        localStorage.setItem('blogPostBvs', JSON.stringify(temp));      // Заносим масив в локалсторадже под названием blogPostBvs
+    }
+    // Метод Добавление поста
+    AddNewPost = (blogPost) => {
+        this.setState((state) => {
+            const temp = [...this.state.blogArr];                       // копируем сылку массива
+            temp.push(blogPost);                                        // Добавляем в новый масив Новый пост
+            localStorage.setItem('blogPostBvs', JSON.stringify(temp));  // Заносим масив в локалсторадже под названием blogPostBvs
+            return {
+                blogArr: temp,                                          // Заносим новый масив в state масив
+            }
+        })
+    }
+    // Метод удаление поста
+    DeletePost = (blogpost) => {
+        if(window.confirm(`удалить: ${blogpost.title}`)){
+            console.log(blogpost);
+            axios.delete(`https://634fe5de78563c1d82b2e4e2.mockapi.io/posts/${blogpost.id}`)
+                .then(() => {
+                    this.GetPosts();
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+
+            // const temp = [...this.state.blogArr];
+            // temp.splice(id, 1)
+            // this.setState({
+            //     blogArr: temp                                            // Меняем масив state на новый
+            // })
+            // localStorage.setItem('blogPostBvs', JSON.stringify(temp));  // Заносим масив в локалсторадже под названием blogPostBvs
+        }
+    }
+    // Метод Показывание пользователейн поста
     TooglePosts = () => {
         this.setState(state => {
             return{
@@ -48,44 +85,47 @@ export class Blog extends Component {
             }
         })
     }
+    // Вывод страницы
     render() {
         const {showUsers} = this.state;
-        const users = showUsers ? <Col><ListGroup><Users/></ListGroup></Col> : null;
-        const buttonText = showUsers ? 'Скрыть пользователей' : 'Показать пользователей';
-        const blogPosts = this.state.blogArr.map((item, pos) =>{
+        const users = showUsers ? <Col><ListGroup><Users/></ListGroup></Col> : null;        // Проверка если showUsers = True то выводим пользователей
+        const buttonText = showUsers ? 'Скрыть пользователей' : 'Показать пользователей';   // Проверка если showUsers = True то меняем имя кнопки
+        const blogPosts = this.state.blogArr.map((item, id) =>{                 // Проходим по масиву blogArr и выводим компонент с поставим
             return(
                 <Col className='mb-5'>
-                    <PostItem 
+                    <PostItem
                         key={item.id}
                         title={item.title}
                         description={item.description}
                         liked={item.liked}
-                        likePost={()=> this.likePosts(pos)}
-                        DeletePost={()=> this.DeletePost(pos)}
+                        likePost={()=> this.likePosts(item.id)}
+                        DeletePost={()=> this.DeletePost(item)}
                     />
                 </Col>
             )
         })
-
+        const spinner = <Col className="my-4 text-center"><Spinner animation="border" /></Col>;     // Конструкция спинера
+        const loadingPost = (this.state.blogArr.length === 0) ? spinner : blogPosts;                // Проверка пока грзуться API и пустой массив выводим спинер
         return(
             <Container fluid>
                 <h2>Добавить пост</h2>
-                <AddForm />
-                <hr></hr>
+                <AddForm
+                    AddNewPost = {this.AddNewPost}
+                    blogArr={this.state.blogArr}
+                />
+                <hr/>
+
                 <h2>Посты</h2>
                 <Row xs={1} lg={2} gap={3}>
-                    {blogPosts}
+                    {loadingPost}
                 </Row>
                 <Button className='mb-5 d-block' variant="primary">Создать пост</Button>
-                
-                
-                
                 <h2>Пользователи</h2>
                 <Row className='mb-3'>
                     {users}
                 </Row>
                 <Button className='mb-5 d-block' onClick={this.TooglePosts} variant="primary">{buttonText}</Button>
-                <Button className='mb-3 d-block' onClick={() => showGetPosts(posts)} variant="info">Показать кол-во постов</Button>
+                <Button className='mb-3 d-block' onClick={() => showGetPosts(this.state.blogArr)} variant="info">Показать кол-во постов</Button>
                 <Button className='mb-3 d-block' onClick={() => showText('hello')} variant="dark">Показать привет</Button>
             </Container>
         )
--- src/components/Blog/component/AddForm.js
+++ src/components/Blog/component/AddForm.js
@@ -17,17 +17,30 @@ export default class AddForm extends Component {
             textValue: e.target.value
         })
     }
+
+    CreatePost = (e) =>{
+        e.preventDefault();
+        const post = {
+            id: this.props.blogArr.length+1,
+            title: this.state.titleValue,
+            description: this.state.textValue,
+            liked: false
+        }
+        this.props.AddNewPost(post);
+        console.log(post)
+    }
   render() {
     return (
-        <Form>
+        <Form onSubmit={this.CreatePost}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" onChange={this.postTitleChange} placeholder="name@example.com" value={this.state.titleValue}/>
+                <Form.Control type="text" onChange={this.postTitleChange} placeholder="name@example.com" value={this.state.titleValue}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Example textarea</Form.Label>
                 <Form.Control as="textarea" onChange={this.postTextChange} rows={3} value={this.state.textValue}/>
             </Form.Group>
+            <button onClick={this.CreatePost}>12312321</button>
         </Form>
     )
   }
